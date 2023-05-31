25f9d693724e05beeb3602b69fb4a213938a3fea
Zakjanzi <zakjanzi97@gmail.com>
2023-05-31 12:37:15 +0300

    added Bill Pay test case

--- src/test/java/bankTest/Login.java
+++ src/test/java/bankTest/Login.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import java.time.Duration;
 
